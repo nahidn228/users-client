122ca4ab3a48bdd1fde1bbe4a1758450f98ae0a1
Nahid Hasan <nahidn228@gmail.com>
2024-11-23 22:42:24 +0600

    Send Client data to the server and display data on the client

--- src/App.jsx
+++ src/App.jsx
@@ -15,18 +15,23 @@ function App() {
     const form = e.target;
     const name = form.name.value;
     const email = form.email.value;
-    const newUser = { name, email };
+    const user = { name, email };
 
-    console.log(newUser);
+    console.log(user);
     fetch("http://localhost:5000/users", {
       method: "POST",
       headers: {
         "content-type": "application/json",
       },
-      body: JSON.stringify(newUser),
+      body: JSON.stringify(user),
     })
       .then((res) => res.json())
-      .then((data) => setUsers(data));
+      .then((data) => {
+        console.log(data);
+        const newUser = [...users, data];
+        setUsers(newUser);
+        form.reset();
+      });
   };
   return (
     <>
@@ -41,7 +46,7 @@ function App() {
       </form>
 
       <div>
-        {users.map((user, idx) => (
+        {users?.map((user, idx) => (
           <div key={idx}>
             <p>
               {user.id} = {user.name} : {user.email}
